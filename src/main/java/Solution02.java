b598901d1ee1c6219be18fa5552e2e439f0359be
Miguel <miguelangel53013@gmail.com>
2021-09-05 14:28:20 -0400

    Initial commit

---
+++ src/main/java/Solution02.java
@@ -0,0 +1,20 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Miguel Rosario
+ */
+import java.util.*;
+public class Solution02 {
+
+    public static void main(String[] args) {
+	/*
+		print: What is the input string?
+	    'userInput' = read string from user
+		print: 'userInput' has userInput.length characters.
+	*/
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is the input string? ");
+        String userInput = input.nextLine();
+        System.out.println(userInput + " has " + userInput.length() + " characters.");
+    }
+
+}
